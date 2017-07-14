c85968ae448291f090de4d613a078981a50fe9e1
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-07-14 13:26:18 -0700

    Tries, guess += & -=

--- pythonFAB/guessingGame.py
+++ pythonFAB/guessingGame.py
@@ -6,11 +6,13 @@ start = 1
 end = 100
 usr_response = ""
 valid_response = ["Lower", "Higher", "Correct!"]
+tries = 0
 
 
 while usr_response != "Correct!":
     print("Im guessing your number")
     comp_guess = random.randint(start, end)
+    tries += 1
     print("Is your number %s" %(comp_guess))
     print("Please respond with Lower , Higher or Correct!")
     usr_response = input()
@@ -19,6 +21,10 @@ while usr_response != "Correct!":
         print("Lets try again...\n")
         continue
     if usr_response == "Lower":
+        comp_guess -= 1
         end = comp_guess
     if usr_response == "Higher":
+        comp_guess += 1
         start = comp_guess
+    if usr_response == "Correct!":
+        print("\nI guessed your number in %s tries" %(tries))
